964467d26a09d9b29274e3b4e3fdfe5c8b6f6fa3
blueshogun96 <blueshogun96@aim.com>
2015-06-24 16:31:55 -0700

    Meh, can't remember

---
+++ source/KeMetal/KeMetalRenderDevice.h
@@ -0,0 +1,8 @@
+//
+//  KeMetalRenderDevice.h
+//
+//  Created by Shogun3D on 6/24/15.
+//  Copyright (c) 2015 Shogun3D. All rights reserved.
+//
+
+
---
+++ source/KeMetal/KeMetalRenderDevice.mm
@@ -0,0 +1,7 @@
+//
+//  KeMetalRenderDevice.m
+//
+//  Created by Shogun3D on 6/24/15.
+//  Copyright (c) 2015 Shogun3D. All rights reserved.
+//
+
