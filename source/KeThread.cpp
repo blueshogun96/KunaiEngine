ae8adbf8de9572e4abd98860159d394da966618c
blueshogun96 <blueshogun96@aim.com>
2015-07-17 19:40:22 -0700

    More work for threading

--- source/KeThread.cpp
+++ source/KeThread.cpp
@@ -16,13 +16,19 @@
 KeThread::KeThread( KeThreadPfn pfn, void* context = NULL, bool suspended = No )
 {
 #ifndef _WIN32
-	/* Create a thread attribute */
+	/* Create a thread attribute and mutex */
 	pthread_attr_init( &thread_attr );
+	pthread_mutex_init( &mutex );
 
 	/* Create suspended thread if desired */
 	if( suspended ) 
-		pthread_attr_setcreatesuspended_np( &thread_attr );
+#ifdef __APPLE__
+		pthread_create_suspended_np( &thread_attr );
+#endif
 #else
+	/* Create mutex */
+	mutex = CreateMutex( NULL, TRUE, NULL );
+
 	/* Create a thread */
 	thread = (HANDLE) _beginthreadex( NULL, 0, pfn, context, suspended ? CREATE_SUSPENDED : 0, &thread_id );
 #endif
@@ -36,27 +42,34 @@ KeThread::KeThread( KeThreadPfn pfn, void* context = NULL, bool suspended = No )
 KeThread::~KeThread()
 {
 #ifndef _WIN32
-	/* Kill this thread */
+	/* Kill this thread and mutex */
 	pthread_attr_destroy( &thread_attr );
+	pthread_mutex_destroy( &mutex );
+	pthread_kill( thread, 0 );
+	pthread_join( thread, NULL );
 #else
+	/* Close the thread and mutex */
 	CloseHandle( thread );
+	CloseHandle( mutex );
 #endif
 }
 
 
 /*
- * Name: 
- * Desc: 
+ * Name: KeThread::Wait
+ * Desc: Wait for the desired amount of time for this thread to finish. 
+ *		 If successful, return true.  If the wait was cancelled or failed,
+ *	     return false.
  */
 bool KeThread::Wait( uint32_t timeout )
 {
-	/* Wait for the desired amount of time for this thread to finish. 
-	   If successful, return true.  If the wait was cancelled or failed,
-	   return false. */
-
+#ifdef _WIN32
 	last_error = WaitForSingleObject( thread, timeout );
 	if( last_error != WAIT_OBJECT_0 )
 		return false;
+#else
+	
+#endif
 
 	return true;
 }
@@ -69,4 +82,36 @@ bool KeThread::Wait( uint32_t timeout )
 uint32_t KeThread::GetLastError()
 {
 	return last_error;
+}
+
+
+/*
+ * Name: KeThread::Suspend
+ * Desc: Suspends the thread.
+ */
+void KeThread::Suspend()
+{
+#ifdef _WIN32
+	last_error = SuspendThread( thread );
+#elif defined(__APPLE__)
+	thread_suspend( pthread_mach_thread_np( thread ) );
+#else
+#endif
+} 
+
+
+/*
+ * Name: KeThread::REsume
+ * Desc: Resumes the thread.
+ */
+void KeThread::Resume()
+{
+#ifdef _WIN32
+	last_error = ResumeThread( thread );
+#elif defined(__APPLE__)
+	pthread_mutex_lock( mutex );
+	thread_resume( pthread_mach_thread_np( thread ) );
+	pthread_mutex_unlock( mutex );
+#else
+#endif
 }
--- source/KeThread.h
+++ source/KeThread.h
@@ -12,12 +12,16 @@
 
 
 /*
- * Thread function pointer
+ * OS specific defines
  */
 #ifndef _WIN32
 typedef void (*KeThreadPfn)( void* );
+
+#define KeExitThread() pthread_exit(NULL)
 #else
 typedef uint32_t (__stdcall *KeThreadPfn)( void* );
+
+#define KeExitThread() ExitThread(0)
 #endif
 
 
@@ -31,12 +35,14 @@ struct KeThread
 
 	bool Wait( uint32_t timeout );
 	uint32_t GetLastError();
+	void Suspend(), Resume();
 
 #ifndef _WIN32
 	pthread_attr_t thread_attr;
 	pthread_t thread;
+	pthread_mutex_t mutex;
 #else
-	HANDLE thread;
+	HANDLE thread, mutex;
 	uint32_t thread_id;
 #endif
 	uint32_t last_error;
